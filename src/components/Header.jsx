d68b35b2c2f0fd9df41276c4e3af5da368baf0d1
Phanindra Allamsetty <phani.allamsetty@gmail.com>
2025-02-28 18:09:24 -0500

    Styling changes to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <>
             <header>
                 <nav>
-                    <img src="/src/assets/globe.png" />
+                    <img src="/src/assets/globe.png" alt="globe icon" />
                     <span>My Travel Journal</span>
                 </nav>
             </header>
