74ffed409636ce4b40bf9229a408d54b76a29863
Phanindra Allamsetty <phani.allamsetty@gmail.com>
2025-02-28 18:00:45 -0500

    Created the header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <nav>
+                    <img src="/src/assets/globe.png" />
+                    <span>My Travel Journal</span>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header
